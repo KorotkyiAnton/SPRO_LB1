4169b4ec4e4d70fd370398eda0ae878d5376b66b
Anton Korotkyi <anton.korotkyi@nure.ua>
2021-10-15 18:48:33 +0300

    hfhgh

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -3,11 +3,12 @@
 #include "time.h"
 #include <string>
 
-HINSTANCE hInst;   
+HINSTANCE hInst;
 LPCTSTR szWindowClass = "Korotkyi Anton";
 LPCTSTR szTitle = "Korotkyi Anton";
 int first_numb, second_numb, sub_numb;
-
+BOOL first = false;
+BOOL second = false;
 
 ATOM MyRegisterClass(HINSTANCE hInstance);
 BOOL InitInstance(HINSTANCE, int);
@@ -32,41 +33,41 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 ATOM MyRegisterClass(HINSTANCE hInstance) {
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW, CS_VREDRAW;     
-	wcex.lpfnWndProc = (WNDPROC)WndProc;    
+	wcex.style = CS_HREDRAW, CS_VREDRAW;
+	wcex.lpfnWndProc = (WNDPROC)WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;    
-	wcex.hIcon = LoadIcon(NULL, IDI_HAND);     
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);   
-	wcex.hbrBackground = CreateSolidBrush(RGB(255, 182, 153)); 
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_HAND);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = CreateSolidBrush(RGB(255, 182, 153));
 	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = szWindowClass;     
+	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = NULL;
 
-	return RegisterClassEx(&wcex);      
+	return RegisterClassEx(&wcex);
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
 	HWND hWnd;
-	hInst = hInstance; 
-	hWnd = CreateWindow(szWindowClass,   
-		szTitle,         
+	hInst = hInstance;
+	hWnd = CreateWindow(szWindowClass,
+		szTitle,
 		WS_OVERLAPPEDWINDOW | WS_CAPTION | WS_BORDER,
 		CW_USEDEFAULT,
-		CW_USEDEFAULT, 
-		1280, 
-		720, 
-		NULL,           
-		NULL,           
-		hInstance,      
-		NULL);         
+		CW_USEDEFAULT,
+		1280,
+		720,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
 
 	if (!hWnd) {
 		return FALSE;
 	}
-	ShowWindow(hWnd, nCmdShow);    
-	UpdateWindow(hWnd);     
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 	return TRUE;
 }
 
@@ -75,16 +76,16 @@ DWORD WINAPI FirstNumbPaint(LPVOID lpParam) {
 	HWND hWnd = (HWND)lpParam;
 	HDC hdc = GetDC(hWnd);
 	RECT rt;
-
-	first_numb = (rand() % 10000) + 1;
-
-	GetClientRect(hWnd, &rt);
-	TextOutA(hdc, 10, 10, std::to_string(first_numb).c_str(), std::to_string(first_numb).size());
-	//while (true) {
-	//	//InvalidateRect(hWnd, NULL, TRUE);
-	//	TextOut(hdc, 10, 10, buff, 5);
-	//	Sleep(500);
-	//}
+	srand(time(0));
+	
+	while (true) {
+		if (!second) {
+			first_numb = (rand() % 10000) + 1;
+			TextOutA(hdc, 10, 10, std::to_string(first_numb).c_str(), 5);
+			first = true;
+			second = false;
+		}
+	}
 	ReleaseDC(hWnd, hdc);
 	return 0;
 }
@@ -94,19 +95,17 @@ DWORD WINAPI SecondNumbPaint(LPVOID lpParam) {
 	HWND hWnd = (HWND)lpParam;
 	HDC hdc = GetDC(hWnd);
 	RECT rt;
-
-	second_numb = (rand() % 10000) + 1;
-
-	GetClientRect(hWnd, &rt);
-	TextOutA(hdc, 10, 10, std::to_string(second_numb).c_str(), std::to_string(second_numb).size());
-	//while (true) {
-	//	//InvalidateRect(hWnd, NULL, TRUE);
-	//	TextOut(hdc, 600, 10, buff, 5);
-	//	Sleep(500);
-	//}
+	while (true) {
+		if (first) {
+			second_numb = (rand() % 10000) + 1;
+			TextOutA(hdc, 10, 30, std::to_string(second_numb).c_str(), 5);
+			second = true;
+			first = false;
+		}
+	}
 	ReleaseDC(hWnd, hdc);
 	return 0;
-	
+
 }
 
 DWORD WINAPI SubNumbPaint(LPVOID lpParam) {
@@ -115,20 +114,16 @@ DWORD WINAPI SubNumbPaint(LPVOID lpParam) {
 	HDC hdc = GetDC(hWnd);
 	RECT rt;
 
-	sub_numb = first_numb - second_numb;
-
-
-	GetClientRect(hWnd, &rt);
-	TextOutA(hdc, 10, 10, std::to_string(sub_numb).c_str(), std::to_string(sub_numb).size());
-	InvalidateRect(hWnd, NULL, FALSE);
-	//while (true) {
-	//	//InvalidateRect(hWnd, NULL, TRUE);
-	//	TextOut(hdc, 900, 10, buff, 5);
-	//	Sleep(500);
-	//}
+	while (true) {
+		if (!first&&second) {
+			sub_numb = first_numb - second_numb;
+			TextOutA(hdc, 10, 50, std::to_string(sub_numb).c_str(), 5);
+			second = false;
+		}
+	}
 	ReleaseDC(hWnd, hdc);
 	return 0;
-	
+
 }
 
 
